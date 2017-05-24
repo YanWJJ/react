026f080f1ad59d0f02dda122ee7bb4290ec40cfb
YanWJJ <1792267386@qq.com>
2017-05-24 14:54:36 +0800

    react_2.0

---
+++ dome9/CommentBox.js
@@ -0,0 +1,10 @@
+let CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+			Hello, world! I am a CommentBox.
+			</div>
+			);
+		}
+	});
+React.render(<CommentBox />,document.getElementById('content'));
